ce87409c6eebaeb5555c727c1d2b9d0d97201f04
Khandakar227 <xenonknight54@gmail.com>
2024-12-07 16:15:35 +0600

    f commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Krishi Dishari
+
+## Installation
+`python version: 3.10`
+Go to `backend`. create a virtual environment.\
+
+```bash
+python -m venv venv
+```
+Activate the environment (For Windows).
+```bash
+venv/Scripts/activate
+```
+
+Install all the python packages.
+```bash
+pip install -r requirements.txt
+```
+
+Run the backend:
+```bash
+fastapi dev main.py
+```
+
+### Add the Model weights
+Download the [ZIP](https://drive.google.com/file/d/1ZuSwo6fr0TEUN7Qfksz6B_vbNNbQHEBP/view?usp=drive_link) file and extract it to `backend/weights` folder.
---
+++ backend/main.py
@@ -0,0 +1,116 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import numpy as np
+import pickle
+
+crop_optimal_model_path = 'weights/Crop Optimal Requirements/dtc_model.pkl'
+crop_suitability_model_path = 'weights/Crop Suitability Geospatial/models_crops_mappings.pkl'
+# Load the model and label encoder
+try:
+    with open(crop_optimal_model_path, 'rb') as model_file:
+        crop_optimal_classifier, crop_optimal_label_encoder = pickle.load(model_file)
+
+    with open(crop_suitability_model_path, 'rb') as model_file:
+        crop_suitability_model, crops, suitability_mapping = pickle.load(model_file)
+    
+except FileNotFoundError:
+    raise RuntimeError("Model file 'dtc_model.pkl' not found. Please ensure the file is present.")
+except pickle.UnpicklingError:
+    raise RuntimeError("Error occurred while loading the model. The file may be corrupted or incompatible.")
+
+# FastAPI app instance
+app = FastAPI()
+
+# Define input schema
+class CropPredictionInput(BaseModel):
+    # medium (50-150 cm), deep (>>150 cm), shallow (20-50 cm)
+    soilDepth: str
+    # medium, light, heavy, organic, wide
+    soilTexture: str
+    # moderate, high, low
+    soilFertility: str
+    # low (<4 dS/m), medium (4-10 dS/m), high (>10 dS/m)), none
+    soilSalinity: str
+    # low (<4 dS/m), medium (4-10 dS/m), high (>10 dS/m)), none
+    soilDrainage: str
+    temperature: float
+    rainfall: float
+    # very bright, clear skies, cloudy skies, light shade, heavy shade
+    lightIntensity: str
+    # 0 - 14
+    soilPH: float
+
+
+# Define route for predicting crops
+@app.post("/predict-crop")
+async def predict_crop(input_data: CropPredictionInput):
+    try:
+        # Prepare the input data
+        sample = np.array([
+            input_data.soilDepth,
+            input_data.soilTexture,
+            input_data.soilFertility,
+            input_data.soilSalinity,
+            input_data.soilDrainage,
+            input_data.temperature,
+            input_data.rainfall,
+            input_data.lightIntensity,
+            input_data.soilPH
+        ])
+
+        # Non-numeric column indices
+        non_numeric_cols = [0, 1, 2, 3, 4, 7]
+
+        # Transform non-numeric values using label encoder
+        for idx in non_numeric_cols:
+            try:
+                transformed_val = crop_optimal_label_encoder.transform([sample[idx]])
+                sample[idx] = transformed_val[0]
+            except Exception as e:
+                raise HTTPException(status_code=400, detail=f"Invalid input for column {idx}: {sample[idx]}.")
+
+        # Convert sample to the correct type for prediction
+        sample = sample.astype(float)
+
+        # Predict the crop
+        prediction = crop_optimal_classifier.predict([sample])[0]
+
+        # Return the predicted crop
+        return {"predicted_crop": prediction}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error occurred during prediction: {str(e)}")
+
+
+
+# Define input schema for geospatial coordinates
+class GeospatialCoordinates(BaseModel):
+    lon: float
+    lat: float
+
+# Define route for predicting crop suitability
+@app.post("/predict-suitability")
+async def predict_suitability(coordinates:GeospatialCoordinates):
+    try:
+        # Predict the crop suitability
+        prediction = crop_suitability_model.predict([[coordinates.lon, coordinates.lat]])
+        crop_suitability = []
+
+        for i, crop in enumerate(crops):
+            # Get the predicted suitability value for the current crop
+            predicted_value = int(prediction[0][i])
+            # Map the predicted integer back to the suitability label
+            suitability_label = [k for k, v in suitability_mapping.items() if v == predicted_value][0]
+            # Append the crop name and suitability label to the list
+            crop_suitability.append((crop, suitability_label))
+
+        
+        crop_suitability_array = np.array(crop_suitability)
+
+        # Return the predicted crop suitability
+        return {"predicted_suitability": crop_suitability_array}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error occurred during prediction: {str(e)}")
+    
+
