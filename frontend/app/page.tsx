55e9207c9c1f69b923c37f18b2fdde5f9433274a
Anonna <meftahul@iut-dhaka.edu>
2024-12-08 03:58:02 +0600

    chatbot added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,15 @@
+import ChatComponent from "@/components/ChatComponent";
 import Navbar from "@/components/common/Navbar";
 import FeaturesSection from "@/components/FeaturesSection";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative"> 
       <Navbar/>
       <HeroSection/>
       <FeaturesSection />
+      <ChatComponent/>
     </div>
   );
 }
